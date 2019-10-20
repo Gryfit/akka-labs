a6ba8e1a548c72a8dfefb7d76345741d20bf83c7
bartek <bartektonia@gmail.com>
2019-10-20 21:00:09 +0200

    lab2

--- src/main/scala/EShop/lab2/CheckoutFSM.scala
+++ src/main/scala/EShop/lab2/CheckoutFSM.scala
@@ -1,6 +1,6 @@
 package EShop.lab2
 
-import EShop.lab2.Checkout.{Data, Uninitialized}
+import EShop.lab2.Checkout._
 import EShop.lab2.CheckoutFSM.Status
 import akka.actor.{ActorRef, LoggingFSM, Props}
 
@@ -31,27 +31,37 @@ class CheckoutFSM extends LoggingFSM[Status.Value, Data] {
   startWith(NotStarted, Uninitialized)
 
   when(NotStarted) {
-    ???
+    case Event(StartCheckout, _) =>
+      goto(SelectingDelivery) using Uninitialized
   }
 
-  when(SelectingDelivery) {
-    ???
+  when(SelectingDelivery, stateTimeout = checkoutTimerDuration) {
+    case Event(SelectDeliveryMethod(method), Uninitialized) =>
+      goto(SelectingPaymentMethod) using Methods(delivery = Some(method), payment = None)
+    case Event(StateTimeout | CancelCheckout, _) =>
+      goto(Cancelled)
   }
 
-  when(SelectingPaymentMethod) {
-    ???
+  when(SelectingPaymentMethod, stateTimeout = checkoutTimerDuration) {
+    case Event(SelectPayment(method), methods: Methods) =>
+      goto(ProcessingPayment) using Methods(delivery = Some(method), payment = methods.payment.orElse(None))
+    case Event(CancelCheckout | StateTimeout, _) =>
+      goto(Cancelled)
   }
 
-  when(ProcessingPayment) {
-    ???
+  when(ProcessingPayment, stateTimeout = paymentTimerDuration) {
+    case Event(ReceivePayment, _) =>
+      goto(Closed)
+    case Event(CancelCheckout | StateTimeout, _) =>
+      goto(Cancelled)
   }
 
   when(Cancelled) {
-    ???
+    case Event(_, _) => goto(Cancelled)
   }
 
   when(Closed) {
-    ???
+    case Event(_, _) => goto(Closed)
   }
 
 }
